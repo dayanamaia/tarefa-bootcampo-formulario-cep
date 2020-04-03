54e8e7c8c06628142730cf83781676aaba8edc16
Dayana Maia <dayrmaia@gmail.com>
2020-04-03 16:52:58 -0300

    Updates

---
+++ precache-manifest.c2f478d8f6bb5b17447fc1f16de1ec13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd9ed8b5eaf996abf5f7df1ef2294c39",
+    "url": "/tarefa-bootcampo-formulario-cep/index.html"
+  },
+  {
+    "revision": "a30fb7b241c4519c57e4",
+    "url": "/tarefa-bootcampo-formulario-cep/static/css/main.8e7da793.chunk.css"
+  },
+  {
+    "revision": "e5520aa9746fa530efa7",
+    "url": "/tarefa-bootcampo-formulario-cep/static/js/2.52f097a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tarefa-bootcampo-formulario-cep/static/js/2.52f097a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a30fb7b241c4519c57e4",
+    "url": "/tarefa-bootcampo-formulario-cep/static/js/main.19534cf5.chunk.js"
+  },
+  {
+    "revision": "73ac65f891fcd37503fa",
+    "url": "/tarefa-bootcampo-formulario-cep/static/js/runtime-main.35196061.js"
+  }
+]);
